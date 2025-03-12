76b33308d1aba64e0a5c6e20d886309f0d50a06a
viktor-trump <viktorkozyr2@gmail.com>
2025-03-12 21:49:21 +0300

    task1

--- TestValues.py
+++ TestValues.py
@@ -6,3 +6,10 @@ F = 100
 transformation = (F - 32) * 5 / 9
 C = round(transformation, 2)
 print(C)
+
+# Задача 1
+
+grams = 2343
+transformation = grams // 1000
+full_killograms = transformation
+print('В', grams, 'граммах', full_killograms, 'килограмма')
