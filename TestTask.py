3b7c8e7bc195c1dd621344a4a98b1fd8ed46f9ea
viktor-trump <viktorkozyr2@gmail.com>
2025-03-12 22:12:58 +0300

    task2

--- TestTask.py
+++ TestTask.py
@@ -13,3 +13,20 @@ grams = 2343
 transformation = grams // 1000
 full_killograms = transformation
 print('В', grams, 'граммах', full_killograms, 'килограмма')
+
+# Задача 2
+
+number = 123
+print(number %10)
+
+# Задача 3
+
+number_1 = -4
+is_positive = number_1 > 0
+is_even = (number_1 % 2 == 0)
+if is_even and is_positive:
+    print("Число", number_1, "является положительным и четным")
+if not is_even:
+    print("Число", number_1, "не подходит под условия")
+if is_even and not is_positive:
+    print("Число", number_1, "является отрицательным и четным")
