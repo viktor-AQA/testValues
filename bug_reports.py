147da08663c2d32a720678b51c16dff3249bf3a0
v.kozyrev <viktor.kozyrev@simbirsoft.com>
2025-03-19 13:43:16 +0300

    edit

--- bug_reports.py
+++ bug_reports.py
@@ -10,8 +10,8 @@ bugs.append(input("Заполни баг-репорт по маске: "))
 bugs.append(input("Заполни баг-репорт по маске: "))
 
 new_list = []
-index = 0
-for bugs[index] in bugs:
+
+for index, bug in enumerate(bugs):
     if 'Low' in bugs[index]:
         continue
     else: new_list.append(bugs[index])
@@ -20,18 +20,17 @@ for bugs[index] in bugs:
 print(new_list)
 
 bugs_finish = []
-index_2 = 0
-for new_list[index_2] in new_list:
-    if 'High' in new_list[index_2]:
-        bugs_finish.append(new_list[index_2])
-    index_2 += 1
+for index, bug in enumerate(new_list):
+    if 'High' in new_list[index]:
+        bugs_finish.append(new_list[index])
+    index += 1
 
 print(bugs_finish)
 
 index_3 = 0
-for new_list[index_3] in new_list:
-    if 'Middle' in new_list[index_3]:
-        bugs_finish.append(new_list[index_3])
-    index_3 += 1
+for index, bug in enumerate(new_list):
+    if 'Middle' in new_list[index]:
+        bugs_finish.append(new_list[index])
+    index += 1
 
 print(bugs_finish)
